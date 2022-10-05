a1dc05cc0fc4800e140cb8dfd2af04e49bc133b3
Walker Dick <walkerraydick2@gmail.com>
2022-10-05 13:26:56 -0500

    added missing files

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+console.log("This is coming from script.js");
+setInterval(checkReload,1000)
+function checkReload(){
+    fetch("http://localhost/reload")
+.then(response =>{
+    if (!response.ok){
+        console.error(`Request failed with status ${response.status}`)
+    }
+    return response.json()
+
+})
+.then(data => {
+    //console.log(data.reload)
+    if(data.reload == "true"){
+        console.log("Reloading page")
+        location.reload()
+    }
+})
+}
