2e96c1fcf95352b399f9d3d37ea378912799a485
Walker Dick <walkerraydick2@gmail.com>
2022-10-06 11:15:11 -0500

    ugh

--- public/js/reload.js
+++ public/js/reload.js
@@ -1,24 +1,29 @@
+    
+ 
+
+
 // A $( document ).ready() block.
-$(document).ready(function () {
-  console.log("ready!");
+$( document ).ready(function() {
+      console.log("ready!");
   var song = document.getElementById("song").innerText;
   song = song.split("Song")[1];
   let artist = document.getElementById("artist").innerText;
   artist = artist.split("Artist")[1];
-  let title = "Now Playing - " + song + artist;
+  let title = "Now Playing - " + song + "By: " + artist;
   localStorage.setItem("webtitle", title);
   document.title = localStorage.getItem("webtitle");
+ var msg = localStorage.getItem("webtitle")
+var chars = Array.from(msg);
 
-  (function (c, a, m) {
-    let webtitle = localStorage.getItem("webtitle")
-    var title = (c || document.title) + " " + (a || "-") + " ";
-    setInterval(function () {
-        title = title.substring(1) + title.charAt(0);
-        document.title = title;
-    }, m || 300);
-})(
-  /* Tab Text */ webtitle,
-  /* Title Repeat Separator */ "-",
-  /* Scroll Speed (in milleseconds) */ 300
-);
+  function scrollTitle() {
+    chars.push(chars.shift());
+    document.title = chars.join("");
+    window.setTimeout(scrollTitle, 120);
+  }
+
+  (function() {
+    scrollTitle();
+  })();
 });
+
+
