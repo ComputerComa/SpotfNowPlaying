1ee7c5d18ad00d24221ed86e1cdf26a8d8f0f0d7
Walker Dick <walkerraydick2@gmail.com>
2022-10-06 11:05:26 -0500

    more fixes maybe>

--- public/js/reload.js
+++ public/js/reload.js
@@ -1,7 +1,24 @@
-        let song = document.getElementById("song").innerText
-        song = song.split("Song")[1]
-        let artist = document.getElementById("artist")[1]
-        artist = artist.split("Artist")[1]
-        let title = "Now Playing - " + song + artist
-        localStorage.setItem("webtitle",title)
-        document.title = localStorage.getItem("webtitle")
+// A $( document ).ready() block.
+$(document).ready(function () {
+  console.log("ready!");
+  var song = document.getElementById("song").innerText;
+  song = song.split("Song")[1];
+  let artist = document.getElementById("artist")[1];
+  artist = artist.split("Artist")[1];
+  let title = "Now Playing - " + song + artist;
+  localStorage.setItem("webtitle", title);
+  document.title = localStorage.getItem("webtitle");
+
+  (function (c, a, m) {
+    let webtitle = localStorage.getItem("webtitle")
+    var title = (c || document.title) + " " + (a || "-") + " ";
+    setInterval(function () {
+        title = title.substring(1) + title.charAt(0);
+        document.title = title;
+    }, m || 300);
+})(
+  /* Tab Text */ webtitle,
+  /* Title Repeat Separator */ "-",
+  /* Scroll Speed (in milleseconds) */ 300
+);
+});
