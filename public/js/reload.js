b87318659476b2adc5bf92754db6ebf374dce37f
Walker Dick <walkerraydick2@gmail.com>
2022-10-06 11:17:07 -0500

    spacing again

--- public/js/reload.js
+++ public/js/reload.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
   song = song.split("Song")[1];
   let artist = document.getElementById("artist").innerText;
   artist = artist.split("Artist")[1];
-  let title = "Now Playing - " + song + "By: " + artist + "  ";
+  let title = "Now Playing - " + song + " By: " + artist + "  ";
   localStorage.setItem("webtitle", title);
   document.title = localStorage.getItem("webtitle");
  var msg = localStorage.getItem("webtitle")
