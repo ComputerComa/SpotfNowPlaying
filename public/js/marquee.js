c7f140032841b2c65b4b13c543635a8b1c75fe6d
Walker Dick <walkerraydick2@gmail.com>
2022-10-06 10:35:46 -0500

    vix some things and add a dynamic title

---
+++ public/js/marquee.js
@@ -0,0 +1,13 @@
+/* MarqueeTitle v4.0 | MIT License | git.io/vQZbs */
+(function (c, a, m) {
+    
+    var title = (c || document.title) + " " + (a || "-") + " ";
+    setInterval(function () {
+        title = title.substring(1) + title.charAt(0);
+        document.title = title;
+    }, m || 300);
+})(
+  /* Tab Text */ "This webpage has a scrolling tab title",
+  /* Title Repeat Separator */ "-",
+  /* Scroll Speed (in milleseconds) */ 300
+);
